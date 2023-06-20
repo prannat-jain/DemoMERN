df2b95a05b29a086a0c6d2d2066f8e382d0bf962
Prannat Jain <prannatj@gmail.com>
2023-06-20 01:11:32 -0300

    fixed typos, added model schema, api tests work

--- server/server.js
+++ server/server.js
@@ -22,4 +22,14 @@ mongoose
   .then(() => console.log("Connected to mongoDB"))
   .catch(console.error);
 
+//import created model for db
+const SaleInventory = require("./models/SaleInventory");
+
+//make a request to localhost:3001/saleinventory, find the inventory and pass it to the constant as the result
+app.get("/saleinventory", async (req, res) => {
+  const saleinventory = await SaleInventory.find();
+
+  res.json(saleinventory);
+});
+
 app.listen(3001, () => console.log("Server started on port 3001"));
