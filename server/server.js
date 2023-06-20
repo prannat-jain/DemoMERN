c41a99db394f5a4f5d35ac3a99d83bbfaf7d7168
Prannat Jain <prannatj@gmail.com>
2023-06-19 21:28:37 -0300

    setup and connected server with mongoDB

--- server/server.js
+++ server/server.js
@@ -1,1 +1,25 @@
- 
+//handle apis
+const express = require("express");
+//handle db
+const mongoose = require("mongoose");
+//handle cors
+const cors = require("cors");
+
+//invoking express and cors app
+const app = express();
+app.use(express.json());
+
+app.use(cors());
+
+const uri =
+  "mongodb+srv://prannat:prannat.123@cluster0.1wwdrvu.mongodb.net/?retryWrites=true&w=majority";
+
+mongoose
+  .connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to mongoDB"))
+  .catch(console.error);
+
+app.listen(3001, () => console.log("Server started on port 3001"));
