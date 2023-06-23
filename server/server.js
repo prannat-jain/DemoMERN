bc73f195f0ea36f033d948f27cacc44aa76dfef2
Prannat Jain <prannatj@gmail.com>
2023-06-22 23:06:46 -0300

    added code for node.js deployment

--- server/server.js
+++ server/server.js
@@ -66,3 +66,22 @@ app.put("/saleinventoryitem/sold/:itemID", async (req, res) => {
 
   res.json(saleinventoryitem);
 });
+
+//setup to run client from node.js itself using the build
+const path = require("path");
+
+const _dirname = path.dirname("");
+const buildPath = path.join(_dirname, "../client/build");
+
+app.use(express.static(buildPath));
+
+app.get("/*", function (req, res) {
+  res.sendFile(
+    path.join(__dirname, "../client/build/index.html"),
+    function (err) {
+      if (err) {
+        res.status(500).send(err);
+      }
+    }
+  );
+});
